7dc1a08652de99b052358d064665604f36004503
Romain Mrad <romain.mrad@gmail.com>
2024-02-06 13:40:25 +0100

    Renaming JSON keys for better understanding

--- src/parameters.py
+++ src/parameters.py
@@ -7,4 +7,4 @@ with open('../config/market_config.json', 'r') as market_config:
     market_config_data = json.load(market_config)
     target_period: str = market_config_data['Period']  # Possible values: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
     market_symbol: str = market_config_data['MarketIndexSymbol']  # Ticker symbol for market index
-    target_symbols: list[str] = market_config_data['MarketTickerSymbols']  # All ticker symbols for stocks
+    target_symbols: list[str] = market_config_data['StockTickerSymbols']  # All ticker symbols for stocks
